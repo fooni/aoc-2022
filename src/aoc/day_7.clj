bf04295cc15890961b2ea85eb2e227fd0ce6be0b
Pasi Järvilehto <pasi.jarvilehto@gofore.com>
2022-12-09 09:30:24 +0100

    Add day 7 solutions

---
+++ src/aoc/day_7.clj
@@ -0,0 +1,13 @@
+
+(ns aoc.day-7
+  (:require util.input))
+
+
+(defn solution1 [lines] lines)
+(defn solution2 [lines] lines)
+
+(comment
+    ;; 1: 
+  (util.input/with-input-lines "day_7.txt" solution1)
+  ;; 2: 
+  (util.input/with-input-lines "day_7.txt" solution2))
